9f6caa1b07530c4e12ce8290344f7c04846ccd23
Luciano Nunes Fonseca <lucianonunesfonseca@gmail.com>
2020-08-14 19:32:17 -0300

    PUT,DELETE e GET para Cliente

---
+++ src/main/java/nunes/luciano/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,60 @@
+package nunes.luciano.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import nunes.luciano.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Length(min = 5, max = 100, message = "O campo devera ter emtre 5 a 100 caracteres")
+	private String nome;
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Email(message = "Email invalido !")
+	@Length(min = 5, max = 250, message = "O campo devera ter emtre 5 a 250 caracteres")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/nunes/luciano/cursomc/service/ClienteService.java
+++ src/main/java/nunes/luciano/cursomc/service/ClienteService.java
@@ -1,11 +1,17 @@
 package nunes.luciano.cursomc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import nunes.luciano.cursomc.domain.Cliente;
+import nunes.luciano.cursomc.dto.ClienteDTO;
 import nunes.luciano.cursomc.repositories.ClienteRepository;
 import nunes.luciano.cursomc.service.exceptions.ObjectNotFoundException;
 
@@ -19,5 +25,47 @@ public class ClienteService {
 		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id:"+ id
 				+", Tipo: "+ Cliente.class.getName()));
+	}
+	
+	public Cliente Insert(Cliente obj) {
+		obj.setId(null);
+		return clienteRepository.save(obj);
+	}
+	
+	public Cliente Update(Cliente obj) {
+		
+		Cliente newObjt = Find(obj.getId());
+		updateData(newObjt,obj);
+		return clienteRepository.save(newObjt);
+		
+	}
+	
+	public void Delete(Integer id) {
+		Find(id); 
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new nunes.luciano.cursomc.service.exceptions.DataIntegrityViolationException("Não é possivel excluir uma categoria que possui produtos");
+		}
+		
+	}
+	
+	public List<Cliente> FindAll(){
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> finPage(Integer page, Integer linesPerPage,String orderBy, String direction ){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(),null,null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
